e185006a0250e5d5fb7957d99481e97dbd61162a
Jose Uribe <joseuribeh@gmail.com>
2023-06-01 01:32:47 -0500

    Snowpack & Tailwind app-create & API connection

--- src/index.js
+++ src/index.js
@@ -1,6 +1,19 @@
-/**
- * This file is just a silly example to show everything working in the browser.
- * When you're ready to start on your site, clear the file. Happy hacking!
- **/
+const url = 'https://platzi-avo.vercel.app/api/avo'
 
-console.log('Happy hacking :)')
+window
+    .fetch(url)
+    .then(response => response.json())
+    .then(responseJSON => {
+        const allItems = []
+        responseJSON.data.forEach((item) => {
+            const img = document.createElement('img')
+            const title = document.createElement('h2')
+            const price = document.createElement('div')
+            
+            const container = document.createElement('div')
+            container.append(img, title, price)
+
+            allItems.push(container)
+        })
+        document.body.append(...allItems)
+    })
