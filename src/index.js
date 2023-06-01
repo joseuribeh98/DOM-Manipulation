203db282b899c0778bdccd9ba7d0229e19d37d83
Jose Uribe <joseuribeh@gmail.com>
2023-06-01 01:51:31 -0500

    Index.html structure modification

--- src/index.js
+++ src/index.js
@@ -1,19 +1,23 @@
-const url = 'https://platzi-avo.vercel.app/api/avo'
+const baseUrl = 'https://platzi-avo.vercel.app'
+const apiUrl = `${baseUrl}/api/avo`
+const appNode = document.querySelector('#app')
 
 window
-    .fetch(url)
+    .fetch(apiUrl)
     .then(response => response.json())
     .then(responseJSON => {
         const allItems = []
         responseJSON.data.forEach((item) => {
             const img = document.createElement('img')
+            img.src = `${baseUrl}${item.image}`
             const title = document.createElement('h2')
+            title.textContent = item.name
             const price = document.createElement('div')
-            
+            price.textContent = item.price
             const container = document.createElement('div')
             container.append(img, title, price)
 
             allItems.push(container)
         })
-        document.body.append(...allItems)
+        appNode.append(...allItems)
     })
